2d892cf326ff7ffeb1453a589b6220e37c31500e
donlexy <itzdonlexy@gmail.com>
2022-06-08 01:38:09 -0700

    initial commit

---
+++ text.js
@@ -0,0 +1,26 @@
+ //scroll
+     window.addEventListener('scroll', function (){
+        let header = document.querySelector('header');
+        let windowPosition = window.scrollY>0;
+        header.classList.toggle('black', windowPosition);
+})
+
+    const navSlide = () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+
+    //toggle_now 
+    burger.addEventListener('click', () => {
+         nav.classList.toggle('nav-active');      
+    
+    // buger animation
+    burger.classList.toggle('toggle')
+
+    });
+}
+
+
+
+navSlide();
+
